dde53aabc28429bc9dcc7b969356aa58d977b540
nlcdbatheo-ux <nlcdbatheo@gmail.com>
2025-08-15 18:15:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,8 @@ if __name__ == "__main__":
     # Render fournit le PORT via variable d'environnement
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # <--- autorise toutes les origines
